1b21a8ee5f10789053b16ffb309a4c5da8c4aa9b
庞熠明 <995637308@qq.com>
2019-09-10 16:54:20 +0800

    修改

--- 24_随机数.go
+++ 24_随机数.go
@@ -1,9 +1,15 @@
 package main
 import "fmt"
+import "math/rand"
+import "time"
 
 func main ()  {
-
-	
+ 
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < 5; i++ {
+		// rand.Int() 随机一个很大的数
+		fmt.Println("rand = ", rand.Intn(100));
+	}
 	fmt.Println("4>3?", 4>3);
 
 
